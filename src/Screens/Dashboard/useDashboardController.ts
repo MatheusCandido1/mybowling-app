d2376d817df791f6e1e34534dcc0634acb5f4a9d
Matheus <matheus12bh@gmail.com>
2024-07-25 22:17:35 -0700

    load-balls-locations-and-groups-on-start

--- src/Screens/Dashboard/useDashboardController.ts
+++ src/Screens/Dashboard/useDashboardController.ts
@@ -3,12 +3,19 @@ import { useDashboard } from "../../hooks/useDashboard";
 import { useDashboardVersion } from "../../hooks/useDashboardVersion";
 
 import { appVersion } from "../../utils/getAppInfo";
+import { useLocations } from "../../hooks/useLocations";
+import { useBalls } from "../../hooks/useBalls";
+import { useGroups } from "../../hooks/useGroups";
+import { useGroupGetAll } from "../../hooks/useGroupGetAll";
 
 export function useDashboardController() {
   const { handleShowAverageModal, handleShowSplitModal, handleShowAdditionalInformationModal } = useDashboard();
   const { stats, isFetching } = useDashboardGetAll();
   const { apiVersion, isCheckingVersion } = useDashboardVersion();
 
+  const { locations } = useLocations();
+  const { balls } = useBalls();
+  const { groups } = useGroupGetAll();
 
   const hasUpdate = apiVersion.version !== appVersion;
 
